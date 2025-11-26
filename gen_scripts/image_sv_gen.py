df15cb5c606bc41401c58f869a265722ed4d8cf8
Vikram Procter <vikram.procter@gmail.com>
2025-11-26 15:07:48 -0700

    Added Images stuffs

--- gen_scripts/image_sv_gen.py
+++ gen_scripts/image_sv_gen.py
@@ -4,3 +4,14 @@ from PIL import Image
 import sys
 import random
 import copy
+import ImageGen
+
+path = "brent1.png"
+
+file = open("foo.txt")
+
+colour_space = [[0,0,0], [255, 255, 255]]
+
+dithered_img, non_dithered_img = ImageGen.ReturnDitheredImage(path, width=640, height=480, colour=True, colour_space=colour_space, returnNoDitherToo=True)
+
+print(dithered_img)
